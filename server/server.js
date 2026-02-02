7fdc8dc598a6e1dd0a70bac739eea59bb9a6d1e9
Saiuday-gif <saiuday8161@gmail.com>
2026-02-02 08:47:22 +0530

    fixed database connection string

--- server/server.js
+++ server/server.js
@@ -1,24 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
-const connectDB = require('./config/db');
-const menuRoutes = require('./routes/menuRoutes');
+require('dotenv').config(); // Environment variables కోసం
+
 const app = express();
 
-// 1. Database connection
-connectDB();
-
-// 2. Middleware
+// 1. Middlewares
 app.use(cors());
 app.use(express.json());
 
-// 3. Route
-app.use('/api/menu', menuRoutes);
+// 2. MongoDB Connection
+// process.env.MONGO_URI 
+const mongoURI = process.env.MONGO_URI; 
+
+mongoose.connect(mongoURI)
+    .then(() => console.log("MongoDB Atlas Connected Successfully! ✅"))
+    .catch((err) => console.log("Database connection error: ❌", err));
+
+// 3. Simple Route for Testing
 app.get('/', (req, res) => {
-    res.send('Restaurant Menu API is running');
+    res.send('Restaurant Menu API is running...');
 });
 
-// 4. Port setup
-const PORT = 5000;
+app.use('/api/menu', menuRoutes)
+
+
+// 4. Port Setup
+// Render 
+const PORT = process.env.PORT || 5000; 
+
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
