cc9dec73a149e2276388dd588c068255b503c2f6
Saiuday-gif <saiuday8161@gmail.com>
2026-02-02 09:03:31 +0530

    Fixed MongoDB connection and add .env configuration

--- server/server.js
+++ server/server.js
@@ -1,34 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config(); // Environment variables కోసం
+require('dotenv').config(); // Env వేరియబుల్స్ కోసం
 
 const app = express();
 
-// 1. Middlewares
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// 2. MongoDB Connection
-// process.env.MONGO_URI 
-const mongoURI = process.env.MONGO_URI; 
+// MongoDB Connection
+const MONGO_URI = process.env.MONGO_URI; 
 
-mongoose.connect(mongoURI)
-    .then(() => console.log("MongoDB Atlas Connected Successfully! ✅"))
-    .catch((err) => console.log("Database connection error: ❌", err));
+mongoose.connect(MONGO_URI)
+  .then(() => console.log('MongoDB Atlas Connected Successfully! ✅'))
+  .catch((err) => console.log('Database connection error: ❌', err));
 
-// 3. Simple Route for Testing
+// API routes 
 app.get('/', (req, res) => {
-    res.send('Restaurant Menu API is running...');
+  res.send('Restaurant Admin API is running live...');
 });
 
-app.use('/api/menu', menuRoutes)
-
-
-// 4. Port Setup
-// Render 
+// Port Setup
 const PORT = process.env.PORT || 5000; 
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
